1e5cb4fc7cf07154f43f75097c16a3a3ebeeb749
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-10-20 13:34:07 -0400

    Update

---
+++ validate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -z $SCING_HOME ]
+then
+    echo "Environment variable 'SCING_HOME' not defined."
+    exit 1
+fi
+
+java -jar ${SCING_HOME}/devtools/womtool.jar \
+    validate \
+    SpaceRanger.wdl \
+    --inputs ./configs/template.inputs.json

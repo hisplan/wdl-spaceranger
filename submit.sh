0a77de34f402cb2feebed0722618bc5a85829dcc
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2021-10-28 16:08:43 -0400

    Remove unnecessary

--- submit.sh
+++ submit.sh
@@ -29,9 +29,6 @@ then
     exit 1
 fi
 
-rm -rf SpaceRanger.deps.zip
-zip SpaceRanger.deps.zip modules modules/*
-
 cromwell-tools submit \
     --secrets-file ${service_account_key} \
     --wdl SpaceRanger.wdl \
